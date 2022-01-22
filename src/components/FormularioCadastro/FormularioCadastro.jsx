96beca0ac921fd9e21f1842ca5d4af9531bcb48a
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-22 15:31:33 -0400

    usando o bind

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import "./estilo.css"
 
 class FormularioCadastro extends Component {
+    
+    constructor(){
+        super();
+        this.titulo = "";
+    }
+    handleMudancaTitulo(evento){
+        this.titulo = evento.target.value;
+        console.log(this.titulo);
+    }
+
     render() {
         return (
-            <form className="form-cadastro">
+            <form className="form-cadastro"
+            //onSubmit={}
+            >
                 <input
                     type="text"
                     placeholder="Titulo"
                     className="form-cadastro_input"
+                    onChange={this.handleMudancaTitulo.bind(this)}
                 />
                 <textarea rows={15}
                     placeholder="Escreva sua nota..."
