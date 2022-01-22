dcf0822ed7ebbb3374c914e6cf69afb51592f4bf
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-22 15:43:43 -0400

    usando props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import FormularioCadastro from './components/FormularioCadastro/index.js';
 import ListaDeNotas from "./components/ListaDeNotas/index.js"
 
 class App extends Component {
+  
+  criarNota(titulo, texto){
+    console.log(`uma nova nota foi criada `+titulo+`,`+texto);
+  }
+  
   render() {
     return (
       <section className='conteudo'>
-        <FormularioCadastro />
+        <FormularioCadastro criarNota={this.criarNota}/>
         <ListaDeNotas />
       </section>
     );
