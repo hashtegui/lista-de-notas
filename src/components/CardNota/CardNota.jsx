5b3d70da79e99c23d282032877839b04706bd870
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-24 10:29:47 -0400

    Importando svg com reactComponent

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import "./estilo.css"
+import "./estilo.css";
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg";
 
 class CardNota extends Component {
     state = {}
@@ -8,6 +9,7 @@ class CardNota extends Component {
             <section className="card-nota">
                 <header className='card-nota_cabecalho'>
                     <h3 className='card-nota_titulo'>{this.props.titulo}</h3>
+                    <DeleteSVG/>
                 </header>
                 <p className='card-nota_texto'>{this.props.text}</p>
             </section>
